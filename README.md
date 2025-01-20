b6e7f23f79d499522ecf480a0cf69e5fa9d9ba25
GabrielAzevedoprog <142434640+GabrielAzevedoprog@users.noreply.github.com>
2025-01-20 07:31:59 -0300

    Entregando projeto final

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# desafio-amigo-secreto
- 
+# Amigo Secreto - Projeto em JavaScript
+
+Bem-vindo ao projeto **Amigo Secreto**, um projeto desenvolvido para o projeto ONE, sendo ele uma aplicação em JavaScript para sortear amigos. 
+
+## Funções Principais no Jogo:
+
+- **Adicionar amigos:** Insira nomes no campo de entrada para adicionar à lista de participantes.
+- **Validação ao adicionar:** Não é permitido adicionar amigos sem preencher o campo de entrada.
+- **Sorteio de amigos:** Realize o sorteio aleatório entre os amigos adicionados.
+- **Validação do sorteio:** O sorteio só pode ocorrer se houver pelo menos dois amigos na lista.
+
+## Regras do Jogo:
+
+1. **Entrada de dados válida:**
+   - Não é permitido adicionar amigos sem inserir um nome no campo de entrada.
+2. **Número mínimo de participantes:**
+   - O sorteio exige pelo menos dois amigos na lista.
+3. **Lista não vazia para sorteio:**
+   - Não é possível realizar o sorteio com a lista vazia.
+
+## Como Usar
+
+1. **Adicionar Participantes:**
+   - Insira o nome no campo de entrada e clique no botão "Adicionar Amigo".
+   - Certifique-se de que o campo de entrada não esteja vazio.
+
+2. **Realizar Sorteio:**
+   - Após adicionar ao menos dois amigos, clique no botão "Sortear".
+   - O resultado do sorteio será exibido na tela.
+
+3. **Reiniciar o Sorteio:**
+   - Caso deseje realizar um novo sorteio, clique no botão "Adicionar" para limpar a lista de participantes e o resultado se reiniciar.
+
+## Tecnologias Utilizadas
+
+- **HTML5:** Estrutura da aplicação.
+- **CSS3:** Estilização da interface.
+- **JavaScript:** Lógica da aplicação e validações.
+
+
--- app.js
+++ app.js
@@ -1,37 +1,28 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
-let escolhido = Math.floor(Math.random * amigos.length) + 1;
 
-function limparCampo() {
-    let campo = document.querySelector('input');
-    campo.value = '';
-}
-function limparResultado(){
-    let resultadoAmigos = document.getElementById('resultado');
-        resultadoAmigos.innerHTML = '';
+function inserirTexto(tag, texto) {
+    campoTexto = document.getElementById(tag);
+    campoTexto.innerHTML = texto;
 }
 function adicionarAmigo() {
     let amigo = document.querySelector('input').value;
-
     if (amigo == '') {
         alert('Por favor, insira um nome.');
     } else {
         amigos.push(amigo);
         console.log(amigos);
-        limparCampo();
-        limparResultado();
-        let lista = document.getElementById('listaAmigos');
-        lista.innerHTML = amigos;
+        let campoTexto = document.querySelector('input');
+        campoTexto.value = '';
+        inserirTexto('resultado', '')
+        inserirTexto('listaAmigos',amigos)        
     }
 }
-
 function sortearAmigo() {
     if ( amigos.length > 1) {
         let indiceAleatorio = Math.floor(Math.random() * amigos.length);
-        let resultadoAmigos = document.getElementById('resultado');
-        resultadoAmigos.innerHTML = amigos[indiceAleatorio];
-        let lista = document.getElementById('listaAmigos');
-        lista.innerHTML = '';
+        inserirTexto('resultado', 'O amigo sorteado é: ' + amigos[indiceAleatorio] )
+        inserirTexto('listaAmigos', '')
         amigos = [];
     } else {
         alert('Informe no minimo dois amigos para sortear!')
